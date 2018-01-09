4c16c311ceba0d87b385dea9935ca4c808b81592
Erika Weil <erikaweil@Erikas-MacBook-Pro.local>
2018-01-09 11:17:58 -0500

    added associations in models/index

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,8 @@
 const User = require('./user')
-
+const Product = require('./product')
+const Category = require('./category')
+const Cart = require('./cart')
+const Order = require('./order')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -13,6 +16,18 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+User.hasOne(Cart);
+User.hasMany(Order);
+Product.hasMany(Category);
+Product.hasMany(Order);
+Category.hasMany(Product);
+Cart.hasOne(User);
+Cart.hasMany(Product);
+Order.hasOne(Cart);
+Order.hasOne(User);
+
+
 module.exports = {
   User
 }
