07c00088447395c440e1f173475180ba35ab546e
Oczane <oczane.rivera@gmail.com>
2018-01-09 17:40:21 -0500

    removed route to get product by Id and finished route to create a product

--- server/api/products.js
+++ server/api/products.js
@@ -1,33 +1,37 @@
 const router = require('express').Router()
-const {User, Product} = require('../db/models')
+const {User, Product, Category} = require('../db/models')
 module.exports = router
 
+
+//GET ALL PRODUCTS
 router.get('/', (req, res, next) => {
-    Product.findAll()
+    Product.findAll({include: [Category]})
       .then( foundProducts => res.json(foundProducts))
       .catch(next)
 })
 
+//GET PRODUCT BY NAME
 router.get('/:name', (req, res, next) => {
-  Product.findOne({ where: {name: req.params.name}})
+  Product.findOne({include: [Category], where: {name: req.params.name}})
     .then( foundProduct => res.json(foundProduct))
     .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-  Product.findById(req.params.id)
-    .then( foundProduct => res.json(foundProduct))
-    .catch(next)
-})
-
+//CREATE A PRODUCT
 router.post('/:name', (req, res, next) => {
-
+    Product.create(req.body)
+      .then( createdProduct => Product.findById( createdProduct.id, {include: [Category]}))
+      .then( foundProduct => res.json(foundProduct))
+      .catch(next)
 })
 
+
+//UPDATE A PRODUCT BY NAME
 router.put('/', (req, res, next) => {
 
 })
 
+//DELETE A PRODUCT BY ID
 router.delete('/', (req, res, next) => {
 
 })
