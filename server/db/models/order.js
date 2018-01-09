b30a4fc7a8e5147d00988e3e76d4c77e1f16aa1f
Oczane <oczane.rivera@gmail.com>
2018-01-09 11:20:19 -0500

    Consolidated spacing

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,33 +2,15 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  subtotal: {
-    type: Sequelize.INTEGER
-  },
-  total: {
-    type: Sequelize.INTEGER
-  },
-  shippingCost: {
-    type: Sequelize.INTEGER
-  },
-  tax: {
-    type: Sequelize.INTEGER
-  },
-  shippingAddress: {
-    type: Sequelize.STRING
-  },
-  billingAddress: {
-    type: Sequelize.STRING
-  },
-  creditCardNumber: {
-    type: Sequelize.INTEGER
-  },
-  CCV: {
-    type: Sequelize.INTEGER
-  },
-  expirationDate: {
-    type: Sequelize.DATE
-  }
+  subtotal: Sequelize.INTEGER,
+  total: Sequelize.INTEGER,
+  shippingCost: Sequelize.INTEGER,
+  tax: Sequelize.INTEGER,
+  shippingAddress: Sequelize.STRING,
+  billingAddress: Sequelize.STRING,
+  creditCardNumber: Sequelize.INTEGER,
+  creditCardCCV: Sequelize.INTEGER,
+  creditCardExpDate: Sequelize.DATE
 })
 
 module.exports = Order
