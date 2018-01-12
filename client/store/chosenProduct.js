4a561d06ea367d474fe3278f1ad64466b8d2c9fc
Oczane <oczane.rivera@gmail.com>
2018-01-12 04:14:53 -0500

    (FEATURE) STORE/CHOSENPRODUCT add deleteChosenProduct  action and thunk

--- client/store/chosenProduct.js
+++ client/store/chosenProduct.js
@@ -4,6 +4,7 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const SET_CHOSEN_PRODUCT = 'SET_CHOSEN_PRODUCT'
+const DELETE_CHOSEN_PRODUCT = 'DELETE_CHOSEN_PRODUCT'
 
 /**
  * INITIAL STATE
@@ -14,11 +15,14 @@ const defaultChosenProduct = {}
  * ACTION CREATORS
  */
 export const setChosenProduct = chosenProduct => ({type: SET_CHOSEN_PRODUCT, chosenProduct})
+export const deleteChosenProduct = chosenProduct => ({type: DELETE_CHOSEN_PRODUCT, chosenProduct})
 
 /**
  * THUNK CREATORS
  */
-
+export const deleteChosenProductFromDb = chosenProduct => dispatch => axios.delete(`api/products/${chosenProduct.id}`)
+  .then( res => dispatch(deleteChosenProduct(chosenProduct)))
+  .catch( err => console.log(err))
 
 /**
  * REDUCER
@@ -26,6 +30,8 @@ export const setChosenProduct = chosenProduct => ({type: SET_CHOSEN_PRODUCT, cho
 export default function (state = defaultChosenProduct, action) {
   switch (action.type) {
     case SET_CHOSEN_PRODUCT:
+      return action.chosenProduct
+    case DELETE_CHOSEN_PRODUCT:
       return action.chosenProduct
     default:
       return state
