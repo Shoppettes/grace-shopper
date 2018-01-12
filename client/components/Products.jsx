4fe4ba49d5451e1683cd03ebf0e6e841a58128df
Erika Weil <erikaweil@Erikas-MacBook-Pro.local>
2018-01-12 12:05:34 -0500

    rendering all products from db on products component

--- client/components/Products.jsx
+++ client/components/Products.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import {getAllProductsFromDb} from '../store'
+import {connect} from 'react-redux'
+
+/**
+ * import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Router, Route, Switch} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import history from '../history';
+import {Navbar, Sidebar, Footer, Home, Products, SingleProduct, Checkout} from '../components';
+import {fetchCurrentUser, getProductsFromDb} from '../store';
+ */
 // let products = [
 //   {
 //     name: 'Dino bow tie',
@@ -7,50 +17,52 @@ import {getAllProductsFromDb} from '../store'
 //     price: 12.99,
 //     quantity: 82,
 //     isAvailable: true,
-//     imgUrl: '<a href="http://tinypic.com?ref=ae9st0" target="_blank"><img src="http://i67.tinypic.com/ae9st0.jpg" border="0" alt="dinosaur with bow tie"></a>'
+//     imgUrl: '../../public/dino-pics/dino-bow-tie.jpeg'
   
-//   } , {
-//     name: 'Brontasaurus bike',
-//     description: 'This classic dino bike will always be in style! Comes with an adjustable seat for all leg lengths.',
-//     price: 199.99,
-//     quantity: 68,
-//     isAvailable: true,
-//     imgUrl: '<a href="http://tinypic.com?ref=2nrm1rq" target="_blank"><img src="http://i63.tinypic.com/2nrm1rq.jpg" border="0" alt="dinosaur riding bike"></a>'
-//   } , {
-//     name: 'Triceri-top hat',
-//     description: 'Perfect for the gentleman dino, carnivore or herbivore.',
-//     price: 33.99,
-//     quantity: 54,
-//     isAvailable: true, 
-//     imgUrl: '<a href="http://tinypic.com?ref=21ah4rl" target="_blank"><img src="http://i65.tinypic.com/21ah4rl.jpg" border="0" alt="dino stealing top hat"></a>'
-//   } , {
-//     name: 'Tea-rex',
-//     description: 'Entertain your friends or just relax at home with this calming tea blend.',
-//     price: 5.99,
-//     quantity: 52,
-//     isAvailable: true, 
-//     imgUrl: '<a href="http://tinypic.com?ref=2s9twty" target="_blank"><img src="http://i66.tinypic.com/2s9twty.jpg" border="0" alt="dinosaurs drinking tea"></a>'
-//   }, {
-//     name: 'CAKE',
-//     description: 'Perfect for any and all special occasion! Yum!',
-//     price: 19.99,
-//     quantity: 90,
-//     isAvailable: true, 
-//     imgUrl: '<a href="http://tinypic.com?ref=107wgol" target="_blank"><img src="http://i65.tinypic.com/107wgol.jpg" border="0" alt="baby dinosaur eating birthday cake"></a>"></a>'
-//   }
+//   } 
+  // , {
+  //   name: 'Brontasaurus bike',
+  //   description: 'This classic dino bike will always be in style! Comes with an adjustable seat for all leg lengths.',
+  //   price: 199.99,
+  //   quantity: 68,
+  //   isAvailable: true,
+  //   imgUrl: '<a href="http://tinypic.com?ref=2nrm1rq" target="_blank"><img src="http://i63.tinypic.com/2nrm1rq.jpg" border="0" alt="dinosaur riding bike"></a>'
+  // } , {
+  //   name: 'Triceri-top hat',
+  //   description: 'Perfect for the gentleman dino, carnivore or herbivore.',
+  //   price: 33.99,
+  //   quantity: 54,
+  //   isAvailable: true, 
+  //   imgUrl: '<a href="http://tinypic.com?ref=21ah4rl" target="_blank"><img src="http://i65.tinypic.com/21ah4rl.jpg" border="0" alt="dino stealing top hat"></a>'
+  // } , {
+  //   name: 'Tea-rex',
+  //   description: 'Entertain your friends or just relax at home with this calming tea blend.',
+  //   price: 5.99,
+  //   quantity: 52,
+  //   isAvailable: true, 
+  //   imgUrl: '<a href="http://tinypic.com?ref=2s9twty" target="_blank"><img src="http://i66.tinypic.com/2s9twty.jpg" border="0" alt="dinosaurs drinking tea"></a>'
+  // }, {
+  //   name: 'CAKE',
+  //   description: 'Perfect for any and all special occasion! Yum!',
+  //   price: 19.99,
+  //   quantity: 90,
+  //   isAvailable: true, 
+  //   imgUrl: '<a href="http://tinypic.com?ref=107wgol" target="_blank"><img src="http://i65.tinypic.com/107wgol.jpg" border="0" alt="baby dinosaur eating birthday cake"></a>"></a>'
+  // }
 // ];
 
 
-const Products = (products) => {
-  console.log('products', this.state.products)
+const Products = (props) => {
+  const {products} = props;
+  console.log('products ', products)
   return (
     <div>
       <h3>This is the Products component.</h3>
       <div className="row">
-        {products.map(product => (
+        {products.data && products.data.map(product => (
           <div className="col-xs-4" key={product.id}>
             <a className="thumbnail" href="#"> 
-              <img src= {product.imageUrl}/>
+              <img src= {product.imgUrl}/>
               <div className="caption">
                 <h5>
                   <span>{product.name}</span>
@@ -65,4 +77,32 @@ const Products = (products) => {
   )
 };
 
-export default Products;
+const mapState = (state) => {
+  return {
+    products: state.products
+  }
+}
+
+// const mapDispatch = (dispatch) => {
+
+// }
+/**const mapState = (state) => {
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+    isLoggedIn: !!state.auth.currentUser
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadInitialData () {
+      dispatch(fetchCurrentUser())
+      dispatch(getProductsFromDb())
+    }
+  }
+}
+
+// export default connect(null, mapDispatch)(Root) */
+
+export default connect(mapState)(Products);
