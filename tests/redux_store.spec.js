5cfbb75f8d19e558ae73039e3898d3f52e064284
Oczane <oczane.rivera@gmail.com>
2018-01-12 04:03:46 -0500

    (TEST) add thunk creator tests to redux test file

--- tests/redux_store.spec.js
+++ tests/redux_store.spec.js
@@ -3,11 +3,12 @@ import {expect} from 'chai'
 //import actions
 // import store from '../../client/store'
 import {GET_ALL_PRODUCTS, SET_CHOSEN_PRODUCT, GET_ALL_CATEGORIES, SET_CHOSEN_CATEGORY,
-    GET_CART, ADD_TO_CART, DELETE_FROM_CART,  ADD_REVIEW, getAllProducts, setChosenProduct,
+    GET_CART, ADD_TO_CART, DELETE_FROM_CART,  ADD_REVIEW, getAllProducts, setChosenProduct, deleteChosenProduct,
     addToCart
  } from '../client/store'
 
-describe('action creators', () => {
+ describe('Redux architechture', () => {
+  describe('action creators', () => {
     let productsArr, cartArr, newPurchase;
     beforeEach('Create test products array, purchase object, cart array', () => {
         productsArr = [{'name': 'dino nail polish'}]
@@ -18,7 +19,7 @@ describe('action creators', () => {
         it ('returns expected action', () => {
             expect(getAllProducts(productsArr)).to.be.deep.equal({
                 type: 'GET_ALL_PRODUCTS',
-                products: productsArr
+                productsArr
             })
         })
     })
@@ -40,4 +41,21 @@ describe('action creators', () => {
         })
     })
 
-})
+  }) //end Action Creator tests
+
+  describe('thunk creators', () => {
+    let productsArr, chosenProduct;
+    beforeEach('Create test products array, chosenProduct', () => {
+        productsArr = [{'name': 'dino nail polish'}]
+        chosenProduct = {'name': 'dino nail polish'}
+    })
+    describe('deleteChosenProductFromDb', () => {
+      it ('deletes a chosenProduct from the products array', () => {
+        deleteChosenProduct(chosenProduct)
+        productsArr = getAllProducts(productsArr).productsArr
+        expect(chosenProduct).to.be.deep.equal([])
+      })
+    })
+  }) //end thunk creator tests
+ }) //end Redux Archtechture tests
+
