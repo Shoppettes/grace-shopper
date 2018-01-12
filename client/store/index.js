9985ea7f4440908ec9460d7d5035ea4dd5a921d5
Oczane <oczane.rivera@gmail.com>
2018-01-12 11:19:44 -0500

    (FIX) STORE fixing conflicts with the index file

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,10 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
+import products from './products'
 import chosenProduct from './chosenProduct'
 
-const reducer = combineReducers({user, chosenProduct})
+const reducer = combineReducers({user, products, chosenProduct})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
@@ -14,4 +15,5 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './products'
 export * from './chosenProduct'
