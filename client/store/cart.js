7500ceceeef46ec21afc59cfdfd306fb4d2e2ae2
Oczane <oczane.rivera@gmail.com>
2018-01-13 00:06:50 -0500

    (FIX) STORE add and modify update thunk for orderProduct with correct route

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 * ACTION TYPES
 */
 const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART'
+const UPDATE_PRODUCT_AMOUNT = 'UPDATE_PRODUCT_AMOUNT'
 
 /**
 * INITIAL STATE
@@ -14,13 +15,21 @@ const defaultCart = []
 * ACTION CREATORS
 */
 export const addProductToCart = cartProduct => ({type: ADD_PRODUCT_TO_CART, cartProduct})
+export const updateProductAmount = cartProduct => ({type: UPDATE_PRODUCT_AMOUNT, cartProduct})
 
 /**
 * THUNK CREATORS
 */
-export const createOrderProductInstance = orderProductBody => dispatch => axios.post(`api/orderProducts`, orderProductBody)
- .then( res => dispatch(addProductToCart(res.data)))
- .catch( err => console.log(err))
+export const createOrderProductInstance = orderProductBody => dispatch =>
+  axios.post(`api/orderProducts`, orderProductBody)
+    .then( res => dispatch(addProductToCart(res.data)))
+    .catch( err => console.log(err))
+
+ export const updateCartProduct = (orderId, productId, actionToTake) => dispatch =>
+  axios.put(`api/orderProducts/${orderId}/${productId}?${actionToTake}`)
+    .then( res => dispatch(updateProductAmount(res.data)))
+    .catch(err => console.log(err))
+
 
 /**
 * REDUCER
@@ -29,6 +38,11 @@ export default function (state = defaultCart, action) {
  switch (action.type) {
    case ADD_PRODUCT_TO_CART:
      return [...state, action.cartProduct]
+   case UPDATE_PRODUCT_AMOUNT:
+     return state.map( product => {
+       if ( product.productId === action.cartProduct.productId) return action.cartProduct
+       else return product
+     })
    default:
      return state
  }
