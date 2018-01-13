da1ec265cc1dd5a6a9c16adef503eda089d3c4ae
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-12 20:21:56 -0500

    corrected two small syntax errors in Checkout component

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -2,38 +2,39 @@ import React, {Component} from 'react';
 import {connect} from 'redux';
 import {getCurrentOrder} from '../store'
 
-class Checkout extends Component => {
-  constructor(props) {
-    super(props);
-  }
-
+class Checkout extends Component {
   componentDidMount() {
     this.props.getCurrentOrder(); // loads state with currentOrder
     // if currentOrder.id (if exists) use that order to render items in order and all personal information
     // if not, use state.cart to render items in "order" and an empty form for entering information
     // create reducer for currentOrder; create thunk that does a get request for orderId
+
+
+    // maybe two different routes on our Root, one to /orders/checkout/guestcheckout
   }
 
-  return (
-    <div>
-      <span>This is the Checkout component.</span>
-    </div>
-  )
+  render () {
+    return (
+      <div>
+        <span>This is the Checkout component.</span>
+      </div>
+    )
+  }
 };
 
 
 const mapStateToProps = state => {
   return {
-    user: state.currentUser
-    order:
-    isLoggedIn:
+    user: state.currentUser,
+    order: {},
+    isLoggedIn: {}
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     getCurrentOrder () {
-      dispatch(getCurrentOrderFromDb(user.id))
+      dispatch(getCurrentOrder())
     }
   }
 }
