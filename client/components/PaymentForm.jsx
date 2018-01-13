522f4c4a9bdf9201c7a21971f9aa039882dffb01
Erika Weil <erikaweil@Erikas-MacBook-Pro.local>
2018-01-13 13:24:21 -0500

    adding stripe functionality

---
+++ client/components/PaymentForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {connect} from 'react-redux';
+const ReactScriptLoaderMixin = require('react-script-loader').ReactScriptLoaderMixin;
+
+let PaymentForm = React.createClass({ //found this 'createClass' syntax on DavidWalsh blog, not sure 
+    //exactly what it means and if it is outdated
+    mixins: [ReactScriptLoaderMixin],
+
+    getInitialState: function() {
+        return {
+            stripeLoading: true,
+            stripeLoadingError: false,
+            submitDisabled: false,
+            paymentError: null,
+            paymentComplete: false,
+            token: null
+        };
+    },
+
+    getScriptURL: function() {
+        return 'https://js.stripe.com/v2/';
+    },
+
+    onScriptLoaded: function() {
+        if(!PaymentForm.getStripeToken) {
+            Stripe.setPublishableKey('pk_test_4j8nxKms5tgqYitAFPPjBZ01');
+            this.setState({ stripeLoading: false,stripeLoadingError: false});
+        }
+    },
+
+    onScriptError: function() {
+        this.setState({ stripeLoading: false, stripeLoadingError: true})
+    },
+
+    onSubmit: function(event) {
+        
+    }
+
+})
