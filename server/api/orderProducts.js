aa9596c4fa53a84396fea78aaae18b570ad799f3
Oczane <oczane.rivera@gmail.com>
2018-01-13 00:23:47 -0500

    (FIX) API fix post and put routes of orderProduct

--- server/api/orderProducts.js
+++ server/api/orderProducts.js
@@ -19,27 +19,33 @@ router.post('/', (req, res, next) => {
     amount: foundProduct.orderProducts.quantity,
     price: foundProduct.price
   }))
+  .then( newCartProduct => res.status(201).json(newCartProduct))
   .catch(next);
 })
 
 //update the orderProduct instance quantity
 router.put('/:orderId/:productId', (req, res, next) => {
-  OrderProduct.findOrCreate({
-    where: {
-      oderId: req.params.orderId,
-      productId: req.params.productId
-    }
-  })
-    .spread(function(foundOrderProduct, createdOrderProduct){
-        if (foundOrderProduct) return foundOrderProduct
-        else return createdOrderProduct
+    OrderProduct.findOne({
+      where: {
+        orderId: req.params.orderId,
+        productId: req.params.productId
+      }
     })
     .then( orderProduct => {
       if (req.query.increment) orderProduct.incrementQuantity()
       else if (req.query.decrement) orderProduct.decrementQuantity()
+      return Order.findById(orderProduct.orderId)
     })
-    .then( () => res.status(204).json('Update succesful.'))
-    .catch(next)
+    .then( foundProduct => ({
+      orderProductId: foundProduct.orderProducts.id,
+      orderId: foundProduct.orderProducts.orderId,
+      productId: foundProduct.id,
+      productName: foundProduct.name,
+      amount: foundProduct.orderProducts.quantity,
+      price: foundProduct.price
+    }))
+    .then( updatedCartProduct => res.status(201).json(updatedCartProduct))
+    .catch(next);
 })
 
 // increment the quantity of a particular product in a particular order
