426d8bad8b1ca1937788c5acacdc32c42bc0f817
Oczane <oczane.rivera@gmail.com>
2018-01-12 21:27:10 -0500

    (FEATURE) MODELS add id field to orderProduct model

--- server/db/models/orderProduct.js
+++ server/db/models/orderProduct.js
@@ -2,15 +2,20 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const OrderProduct = db.define('OrderProducts', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+},
   quantity: Sequelize.INTEGER
 })
 
 OrderProduct.prototype.decrementQuantity = function () {
-  quantity = this.quantity--;
+  this.quantity = this.quantity--;
 }
 
 OrderProduct.prototype.incrementQuantity = function () {
-  quantity = this.quantity++;
+  this.quantity = this.quantity++;
 }
 
 module.exports = OrderProduct;
