f110ce4b09b17cf8531c64ed0718b9c545d702ae
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-14 11:30:03 -0500

    SingleProduct component receiving correct data for single product and successfully updating state.chosenProduct

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,40 +1,45 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { getChosenProductFromDb } from '../store'
+import {getChosenProductFromDb} from '../store'
 
 
-const singleProduct = (props) => {
-  let { chosenProduct, setProduct} = props;
-  setProduct(props.match.params.id);
-  return (
-    <div>
-      <span>Product Name</span>
-      <div className="col-xs-4" key={props.id}>
-            <a className="thumbnail" href="#">
-              <img src= "" />
-              <div className="caption">
-                <h5>
-                  <span>product.name</span>
-                </h5>
-              </div>
-            </a>
-          </div>
-    </div>
-  )
+class SingleProduct extends Component {
+  componentDidMount () {
+    this.props.loadChosenProduct(this.props.match.params.productId);
+  }
+
+  render () {
+    const {product} = this.props;
+    return (
+      <div>
+        <span>Product Name</span>
+        <div className="col-xs-4" key={product.id}>
+              <a className="thumbnail" href="#">
+                <img src= "" />
+                <div className="caption">
+                  <h5>
+                    <span>{product.name}</span>
+                  </h5>
+                </div>
+              </a>
+            </div>
+      </div>
+    )
+  }
 };
 
-const mapState = ({chosenProduct}) => {
-  return {};
+const mapState = (state) => {
+  return {
+    product: state.chosenProduct
+  }
 };
 
 const mapDispatch = (dispatch) => {
   return {
-    setProduct(productId) {
+    loadChosenProduct(productId) {
       dispatch(getChosenProductFromDb(productId))
     }
   }
 };
 
-export default connect(mapState, mapDispatch)(singleProduct);
-
-
+export default connect(mapState, mapDispatch)(SingleProduct);
