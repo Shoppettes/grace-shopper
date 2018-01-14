066e5223d9a74ea4018210cb6dde48fa19036c43
sazii <stoksari@gmail.com>
2018-01-13 20:33:09 -0500

    small fixes

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Elements} from 'react-stripe-elements'
-import InjectedCheckoutForm from './InjectedCheckoutForm'
+import InjectedCheckoutForm from './InjectedCheckoutForm.jsx'
 
 /**
  * const {
