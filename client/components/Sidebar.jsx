b6937fb56b10bd347aa37eaec2890d2aaf2a22da
Oczane <oczane.rivera@gmail.com>
2018-01-15 01:35:36 -0500

    (STYLING) Add fonts to css folder and modify sidebar css

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Products} from '../components'
 import {Link} from 'react-router-dom'
+const ListGroup = require('react-bootstrap').ListGroup
+const ListGroupItem = require('react-bootstrap').ListGroupItem
 
 export const Sidebar = (props) => {
   const {categories} = props
   return (
-      <div id="sidebar-wrapper">
-        <div className="sidebar-header">
-          <Link to="/">
-          <img src="" alt="modern dino" />
+
+    <div id="sidebar-wrapper">
+    <ListGroup bsClass="list-group">
+      <div className="container-logo">
+        <img className="logo-img" src="./dino-logo.jpg" />
+      </div>
+
+      {categories && categories.map(category => <ListGroupItem bsClass="list-group-item" key={category.id}><Link to="/products">{category.name}</Link></ListGroupItem>)}
+      </ListGroup>
+    </div>
+
+      /*<div id="sidebar-wrapper">
+        <div id="sidebar-header">
+          <Link to="/" >
+          <img src="./dino-logo.jpg" alt="modern dino" height="200" width="200" />
           </Link>
         </div>
         <ul className="sidebar-nav">
@@ -17,7 +30,7 @@ export const Sidebar = (props) => {
             {categories && categories.map(category => <li className="sidebar-brand" key={category.id}><Link to="/products">{category.name}</Link></li>)}
           </div>
         </ul>
-      </div>
+      </div>*/
   )
 };
 
