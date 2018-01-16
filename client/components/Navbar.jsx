2ca7a0bb296d61e1904332a2eb782659cd6461a0
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-01-16 10:55:26 -0500

    conditionally rendering welcome message on navbar when user is logged in

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,18 +6,21 @@ import {logout} from '../store';
 class Navbar extends Component {
   constructor(props) {
     super(props);
+    this.renderWelcome = this.renderWelcome.bind(this);
     this.renderLoginSignup = this.renderLoginSignup.bind(this);
     this.renderLogout = this.renderLogout.bind(this);
   }
 
   render () {
     return (
-
       <div id="nav-bar-wrapper">
         <div className="nav-container">
         <div className="nav-item"></div>
           <div className="nav-item">
           <NavLink to="/products">All Products</NavLink>
+          </div>
+          <div className="nav-item">
+            { this.props.currentUser.id && this.renderWelcome() }
           </div>
           <div className="nav-item">
             { this.props.currentUser.id ? this.renderLogout() : this.renderLoginSignup() }
@@ -27,23 +30,16 @@ class Navbar extends Component {
           </div>
         </div>
       </div>
-      // <Switch>
-        //   {/* Routes placed here are available to all visitors */}
-        //   <Route path="/login" component={Login} />
-        //   <Route path="/signup" component={Signup} />
-        //   {
-        //     isLoggedIn &&
-        //       <Switch>
-        //         {/* Routes placed here are only available after logging in */}
-        //         <Route path="/home" component={UserHome} />
-        //       </Switch>
-        //   }
-        //   {/* Displays our Login component as a fallback */}
-        //   <Route component={Login} />
-        // </Switch>
-
     )
   };
+
+  renderWelcome () {
+    return (
+      <ul className="nav navbar-nav">
+        <li>Welcome {this.props.currentUser.firstName}</li>
+      </ul>
+    )
+  }
 
   renderLoginSignup () {
     return (
@@ -66,9 +62,6 @@ class Navbar extends Component {
       <ul className="nav navbar-nav navbar-right">
         <li>
           <button className="navbar-btn btn btn-default" onClick={this.props.logout}>logout</button>
-        </li>
-        <li>
-          <NavLink to="/cart" activeClassName="active"><img src="/images/cart-icon.jpg" /></NavLink>
         </li>
       </ul>
     );
