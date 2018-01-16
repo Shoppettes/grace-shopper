e19c53712f6830747be44a5e89c0287447130fa5
Erika Weil <erikaweil@Erikas-MacBook-Pro.local>
2018-01-16 14:53:53 -0500

    rendering order info on checkout

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -30,26 +30,16 @@ const onToken = (amount, description, order, handleSuccess) => token =>
       amount: fromDollarToCent(amount)
     })
     .then(() => {
-      console.log('handle success')
-      console.dir(handleSuccess)
       handleSuccess(order)
       alert('Payment successful!')
-      // if (!user.id) {
-      //   axios.put(`/api/orders/${order.id}/newUser`, {email : user.email})
-      // }
-      // else {
-      //   axios.put(`/api/orders/${order.id}`, {orderStatus: 'awaiting shipment'})
-      // } 
-      //I think this logic needs to go in the "handleSuccess" function
     })
     // .catch(() => alert('Payment error'))
 
 /**COMPONENT */
 const Checkout = ({order, handleSuccess}) => {
-  let amount = 100;
-  let description = 'one description for an order';
-  let name = 'Mary sue'
-  console.log('order ', order)
+  let amount = order.products && calcTotal(order.products);
+  let name = 'Your order: ';
+  let description = order.products && createDescription(order.products)
   return (
     <div>
       <StripeCheckout
@@ -66,22 +56,20 @@ const Checkout = ({order, handleSuccess}) => {
     
 
 const createDescription = (cartArr) => { 
-  let description = cartArr.map(cartItem => {
-    return cartItem.description
+ return cartArr.map(cartItem => {
+    return cartItem.name
   }).join(', ');
+}
 
-  console.log(description);
-  return description;
+const calcTotal = (cartArr) => {
+  return cartArr.map(cartProduct => +cartProduct.price).reduce((prev, curr) => prev + curr).toFixed(2);  
 }
 
 
-const mapState = ({currentOrder, products}) => { 
+const mapState = ({currentOrder}) => { 
   return {
-    order: currentOrder,
-    amount: currentOrder.total,
-    products: products,
-    description: createDescription(products)
-
+    order: currentOrder, 
+    products: currentOrder.products
   }
 }
 
@@ -93,75 +81,3 @@ const mapDispatch = dispatch => {
   }
 }
 export default connect(mapState, mapDispatch)(Checkout);
-
-
-/**
- * import React from 'react'
-import { connect } from 'react-redux'
-import axios from 'axios'
-import StripeCheckout from 'react-stripe-checkout'
-import { submitCart } from '../store'
-
-const STRIPE_PUBLISHABLE = 'pk_test_BjdBrWKAZMF0Lwo9Pncz4SxF'
-const PAYMENT_SERVER_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'http://choko-hopper.herokuapp.com/auth/checkout'
-    : 'http://localhost:8080/auth/checkout'
-
-const CURRENCY = 'USD'
-
-const fromUSDToCent = amount => amount * 100
-
-const onToken = (amount, description, handleSuccess, cart) => token =>
-  axios
-    .post(PAYMENT_SERVER_URL, {
-      description,
-      source: token.id,
-      currency: CURRENCY,
-      amount: fromUSDToCent(amount)
-    })
-    .then(() => {
-      handleSuccess(cart)
-      alert('Payment Successful')
-    })
-    .catch(() => alert('Payment Error'))
-
-const Checkout = ({ description, amount, handleSuccess, cart }) => (
-  <div>
-    <StripeCheckout
-      name="Your Chocolate Order"
-      description={description}
-      amount={fromUSDToCent(amount)}
-      token={onToken(amount, description, handleSuccess, cart)}
-      currency={CURRENCY}
-      stripeKey={STRIPE_PUBLISHABLE}
-    />
-  </div>
-)
-
-const findDescription = (cart, products) => {
-  let cartProducts = cart.cart.map(cartItem => {
-    let result = products.find(product => product.id === cartItem.productId)
-    return result ? result.name : result
-  }
-  )
-  return cartProducts.length
-    ? cartProducts.reduce((accumulator, item) => {
-      return accumulator + ', ' + item
-    })
-    : ''
-}
-
-const mapState = ({ products, cart }) => ({
-  products,
-  cart,
-  amount: cart.orderTotal,
-  description: findDescription(cart, products)
-})
-const mapDispatch = dispatch => ({
-  handleSuccess: cart => {
-    dispatch(submitCart(cart))
-  }
-})
-export default connect(mapState, mapDispatch)(Checkout)
- */
